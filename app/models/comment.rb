c2dfc4ac33338c6f007bc8c60f4da69e7d3c772f
adeola003 <khadaf3@gmail.com>
2023-07-13 20:13:42 +0100

    add methods for updating likes and test it

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  
+
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
-  
+
   def update_posts_counter
     update(comments_counter: comments.count)
   end
@@ -10,5 +10,4 @@ class Post < ApplicationRecord
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
-
 end
