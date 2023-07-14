527f5f4eaa8ae3fb0323959e6fe0b8b06eea4e8c
adeola003 <khadaf3@gmail.com>
2023-07-14 13:13:00 +0100

    update like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,10 @@
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post, foreign_key: 'author_id'
+
+  after_save :update_likes_counter
+
+  private
 
   def update_likes_counter
     post.update(likes_counter: post.likes.count)
