03a89a5954cbc4acb65073cda84f4bc02701dec6
adeola003 <khadaf3@gmail.com>
2023-07-14 16:06:56 +0100

    fix rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id' 
+  has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
   def recent_posts(limit = 3)
