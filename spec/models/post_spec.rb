a64092b5f561d7fa4c47777444e14bd22bf6d8a9
adeola003 <khadaf3@gmail.com>
2023-07-14 18:25:48 +0100

    fix rubocop errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,59 +1,35 @@
 RSpec.describe Post, type: :model do
-  describe "validations" do
-    it "requires a title" do
+  describe 'validations' do
+    it 'requires a title' do
       post = Post.new(title: nil, comments_counter: 0, likes_counter: 0)
       expect(post).not_to be_valid
     end
 
-    it "requires comments_counter to be a non-negative integer" do
-      post = Post.new(title: "Example Post", comments_counter: -1, likes_counter: 0)
+    it 'requires comments_counter to be a non-negative integer' do
+      post = Post.new(title: 'Example Post', comments_counter: -1, likes_counter: 0)
       expect(post).not_to be_valid
     end
 
-    it "requires likes_counter to be a non-negative integer" do
-      post = Post.new(title: "Example Post", comments_counter: 0, likes_counter: -1)
+    it 'requires likes_counter to be a non-negative integer' do
+      post = Post.new(title: 'Example Post', comments_counter: 0, likes_counter: -1)
       expect(post).not_to be_valid
     end
-
-    # it "is valid with valid attributes" do
-    #   post = Post.new(title: "Example Post", comments_counter: 0, likes_counter: 0)
-    #   expect(post).to be_valid
-    # end
   end
 
-  describe "associations" do
-    it "belongs to an author" do
+  describe 'associations' do
+    it 'belongs to an author' do
       association = described_class.reflect_on_association(:author)
       expect(association.macro).to eq(:belongs_to)
     end
 
-    it "has many comments" do
+    it 'has many comments' do
       association = described_class.reflect_on_association(:comments)
       expect(association.macro).to eq(:has_many)
     end
 
-    it "has many likes" do
+    it 'has many likes' do
       association = described_class.reflect_on_association(:likes)
       expect(association.macro).to eq(:has_many)
     end
   end
-
-  # describe "callbacks" do
-  #   it "updates the author's post_counter after save" do
-  #     author = User.create(name: "John")
-  #     post = Post.new(title: "Example Post", comments_counter: 0, likes_counter: 0, author: author)
-  #     expect {
-  #       post.save
-  #     }.to change { author.reload.post_counter }.by(1)
-  #   end
-  # end
-
-  # describe "methods" do
-  #   it "returns recent comments for the post" do
-  #     post = Post.create(title: "Example Post", comments_counter: 0, likes_counter: 0)
-  #     comment1 = Comment.create(user: User.new, post: post, text: "Comment 1")
-  #     comment2 = Comment.create(user: User.new, post: post, text: "Comment 2")
-  #     expect(post.recent_comments(2)).to eq([comment2, comment1])
-  #   end
-  # end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,45 +1,35 @@
 RSpec.describe User, type: :model do
-  describe "validations" do
-    it "requires a name" do
+  describe 'validations' do
+    it 'requires a name' do
       user = User.new(name: nil)
       expect(user).not_to be_valid
     end
 
-    it "requires post_counter to be a non-negative integer" do
-      user = User.new(name: "John", post_counter: -1)
+    it 'requires post_counter to be a non-negative integer' do
+      user = User.new(name: 'John', post_counter: -1)
       expect(user).not_to be_valid
     end
 
-    it "is valid with valid attributes" do
-      user = User.new(name: "John", post_counter: 0)
+    it 'is valid with valid attributes' do
+      user = User.new(name: 'John', post_counter: 0)
       expect(user).to be_valid
     end
   end
 
-  describe "associations" do
-    it "has many posts" do
+  describe 'associations' do
+    it 'has many posts' do
       association = described_class.reflect_on_association(:posts)
       expect(association.macro).to eq(:has_many)
     end
 
-    it "has many comments" do
+    it 'has many comments' do
       association = described_class.reflect_on_association(:comments)
       expect(association.macro).to eq(:has_many)
     end
 
-    it "has many likes" do
+    it 'has many likes' do
       association = described_class.reflect_on_association(:likes)
       expect(association.macro).to eq(:has_many)
     end
   end
-
-  # describe "methods" do
-  #   it "returns recent posts for the user" do
-  #     user = User.create(name: "John")
-  #     post1 = Post.create(author: user, title: "Post 1", created_at: 1.day.ago)
-  #     post2 = Post.create(author: user, title: "Post 2", created_at: 2.days.ago)
-
-  #     expect(user.recent_posts(2).to_a).to eq([post1, post2])
-  #   end
-  # end
 end
