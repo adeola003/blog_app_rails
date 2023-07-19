d1a3773ad7e453a34a07ac44c15c37a32dfcc818
adeola003 <khadaf3@gmail.com>
2023-07-19 07:41:31 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'users#index' # All users page
+  get '/users/:id', to: 'users#show', as: 'user' # Single user page
+  get '/users/:user_id/posts', to: 'posts#index_by_user', as: 'user_posts' # All posts by a given user page
+  get '/posts/:id', to: 'posts#show', as: 'post' # Single post page
 end
