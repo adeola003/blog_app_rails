c12c9cf69d810baf867bb37a663c70ec9157ec78
adeola003 <khadaf3@gmail.com>
2023-07-19 18:05:11 +0100

    update spec request

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      get posts_index_path
+    it "returns a successful response" do
+      get user_posts_path(user_id: 1)
       expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get user_posts_path(user_id: 1)
+      expect(response).to render_template(:index_by_user)
+    end
+
+    it "includes the correct placeholder text in the response body" do
+      get user_posts_path(user_id: 1)
+      expect(response.body).to include("All posts by username")
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_index_path
+    it "returns a successful response" do
+      get users_path
       expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it "includes the correct placeholder text in the response body" do
+      get users_path
+      expect(response.body).to include("All Users")
     end
   end
 end
