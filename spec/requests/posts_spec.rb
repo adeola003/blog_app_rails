9392630dcb184a39ec2c1834be4a3c8f6ddd6045
adeola003 <khadaf3@gmail.com>
2023-07-19 19:22:00 +0100

    make requested change

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /posts' do
+  describe 'GET /index' do
     it 'returns a successful response' do
       get user_posts_path(user_id: 1)
       expect(response).to have_http_status(200)
@@ -15,6 +15,23 @@ RSpec.describe 'Posts', type: :request do
     it 'includes the correct placeholder text in the response body' do
       get user_posts_path(user_id: 1)
       expect(response.body).to include('All posts by username')
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns a successful response' do
+      get post_path(1)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      get post_path(1)
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      get post_path(1)
+      expect(response.body).to include('Posts Details')
     end
   end
 end
