651217ce32e58ac933fb5a29b2b0bae3e633b1c7
adeola003 <khadaf3@gmail.com>
2023-07-19 07:52:50 +0100

    add controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+  def index_by_user
+    # Code to fetch all posts by the user with the specified ID and pass them to the view
+  end
+
+  def show
+    # Code to fetch the post with the specified ID and pass it to the view
+  end
+end
