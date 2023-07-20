59d91ca945025d472b5db8f24747ad6a97317300
adeola003 <khadaf3@gmail.com>
2023-07-20 22:07:55 +0100

    implement index and show methods in PostController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def index
     # Code to fetch all users and pass them to the view
+    @users = User.all
   end
 
   def show
     # Code to fetch the user with the specified ID and pass it to the view
+    @user = User.find(params[:id])
   end
 end
