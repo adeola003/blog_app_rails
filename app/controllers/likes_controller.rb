996e94c043838f3874d78f4a94592a04eb5dc96a
adeola003 <khadaf3@gmail.com>
2023-07-22 08:10:28 +0100

    create like controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,14 @@
+# app/controllers/likes_controller.rb
+class LikesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.build(user: current_user)
+
+    if @like.save
+      redirect_to post_path(@post)
+    else
+      flash[:error] = 'Failed to like the post.'
+      redirect_to post_path(@post)
+    end
+  end
+end
