b5e33d60cc3eb3379ab3114da7be19c449bbd87d
adeola003 <khadaf3@gmail.com>
2023-07-27 18:31:27 +0100

    make requested changes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,11 +4,9 @@ class LikesController < ApplicationController
     @like = @post.likes.new(user: current_user)
 
     if @like.save
-      # Update the likes_counter for the associated post
       @post.update(likes_counter: @post.likes.count)
       redirect_to post_path(@post)
     else
-      # Handle errors here, if necessary
       redirect_to post_path(@post), alert: 'Failed to like the post.'
     end
   end
