e1ddff72378d3198c3600b5c81dd284f61949baf
adeola003 <khadaf3@gmail.com>
2023-07-27 16:37:32 +0100

    add tests for post index view

--- spec/features/posts_index_by_user_spec.rb
+++ spec/features/posts_index_by_user_spec.rb
@@ -5,20 +5,22 @@ RSpec.describe 'User Post Index', type: :feature do
 
   before(:each) do
     # Create posts for the user
+    @post1 = user.posts.create(title: 'Post 1', text: 'This is the first post.')
     visit user_posts_path(user)
   end
 
-  it 'displays the user profile picture, username, and number of posts' do
+  it 'displays the username and number of posts' do
     puts user.name
     expect(page).to have_content("All Posts by #{user.name}")
+    expect(page).to have_content("Number of posts: #{user.post_counter}")
   end
 
   it "displays the post's title and part of the post's body" do
-    expect(page).to have_content('This user has no posts yet.')
+    expect(page).to have_content('Post 1')
   end
 
   it 'displays the number of comments and number of likes for each post' do
-    expect(page).to have_content('This user has no posts yet.')
+    expect(page).to have_content('Comments: / Likes:')
   end
 
   it 'redirects to the new post page when clicking "Create New Post"' do
