d2f46bad2e87882dcdec5c5f65f038ec35c449de
adeola003 <khadaf3@gmail.com>
2023-07-29 10:18:54 +0100

    define action to list all users and update routes

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,8 @@
 class Api::PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+    render json: @posts
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   get '/users/:user_id/posts', to: 'posts#index_by_user', as: 'user_posts' # All posts by a given user page
   get '/posts/:id', to: 'posts#show', as: 'post' # Single post page
 #
+namespace :api do
+  resources :users, only: [] do
+    resources :posts, only: [:index]
+  end
+end
+
 resources :users, only: [:index, :show] do
   resources :posts, only: [:index, :show, :new, :create] do
     resources :comments, only: [:create]
